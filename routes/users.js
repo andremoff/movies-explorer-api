0dabc5948746ce49d5203ae1e51d8384d3239eee
Andrey Dremov <andrem0ff@yandex.ru>
2023-07-23 17:56:32 +0300

    Backend

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 const usersRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { getMe, updateMe } = require('../controllers/users');
+const { getCurrentUser, updateUser } = require('../controllers/users');
 
-usersRouter.get('/me', getMe);
+usersRouter.get('/me', getCurrentUser);
 
 usersRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
   }),
-}), updateMe);
+}), updateUser);
 
 module.exports = usersRouter;
