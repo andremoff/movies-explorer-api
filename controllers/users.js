b302bf4ddb08ed51a6fb8be9402cc1e58e155dab
Andrey Dremov <andrem0ff@yandex.ru>
2023-07-31 22:55:00 +0300

    Backend fix

--- controllers/users.js
+++ controllers/users.js
@@ -101,7 +101,7 @@ const login = async (req, res, next) => {
     return res
       .cookie('jwt', token, {
         httpOnly: true,
-        sameSite: 'Strict',
+        sameSite: 'Lax',
         maxAge: 3600000 * 24 * 7,
       })
       .json({ message: 'Авторизация успешна', token });
